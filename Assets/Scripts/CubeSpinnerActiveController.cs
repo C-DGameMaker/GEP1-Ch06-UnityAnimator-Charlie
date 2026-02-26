93088026777d3e035ef7490050b3f1347bd34f88
C-DGameMaker <charlotte5634@outlook.com>
2026-02-26 13:50:30 -0400

    Initial commit

---
+++ Assets/Scripts/CubeSpinnerActiveController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+
+public class CubeSpinnerActiveController : MonoBehaviour
+{
+    public Animator _anim;
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+   
+}
